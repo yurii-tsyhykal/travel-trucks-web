5fa9619d108192c099dce770ef6c923207a5c141
Yurii <y.tsyhykal@gmail.com>
2024-12-18 00:43:34 +0100

    added form init setup/started styles

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,17 @@
 export const CONTAINER_VALUES = {
   heroImg: 'heroImg',
   heroText: 'heroText',
+  catalogForm: 'catalogForm',
+};
+
+export const FORMS_ID = {
+  location: 'location',
+  ac: 'ac',
+  automatic: 'automatic',
+  kitchen: 'kitchen',
+  tv: 'tv',
+  bathroom: 'bathroom',
+  van: 'van',
+  fully: 'fully',
+  alcove: 'alcove',
 };
--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,10 +1,14 @@
 import CampersList from '../components/CampersList/CampersList';
+import CatalogForm from '../components/CatalogForm/CatalogForm';
 import Container from '../components/Container/Container';
+import { CONTAINER_VALUES } from '../constants';
 
 const CatalogPage = () => {
   return (
     <>
-      <Container></Container>
+      <Container container={CONTAINER_VALUES.catalogForm}>
+        <CatalogForm />
+      </Container>
       <Container>
         <CampersList />
       </Container>
